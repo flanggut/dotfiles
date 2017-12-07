f8c5f3726e74a3ccd7e0e2405a8a9fd71f26ea0f
Fabian Langguth <fabian.langguth@gmail.com>
2017-12-07 13:55:26 +0100

    fish: update fzf commands

--- omf/functions/fzf_reverse_history_search.fish
+++ omf/functions/fzf_reverse_history_search.fish
@@ -1,5 +1,5 @@
 function fzf_reverse_history_search
-  history | fzf --no-sort | read -l command
+  history | fzf --reverse --no-sort | read -l command
   if test $command
     commandline -rb $command
   end
--- omf/functions/zd.fish
+++ omf/functions/zd.fish
@@ -1,5 +1,5 @@
 function zd
-  set dir (z -l $argv | cut -b12- | fzf --preview 'tree -C {} | head -200')
+  set dir (z -l $argv | cut -b12- | tail -r | fzf --reverse --preview 'tree -C {} | head -200')
   if [ $dir ]
     cd $dir
   end
