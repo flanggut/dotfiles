719a05892ba8fb7d24dc189df08102671291f89f
Fabian Langguth <fabian.langguth@gmail.com>
2018-05-30 21:27:29 -0700

    Several updates

--- omf/init.fish
+++ omf/init.fish
@@ -1,9 +1,14 @@
+set -g theme_color_scheme solarized
+set -g theme_date_format "+%H:%M:%S"
+
+set -gx BD_OPT 'insensitive'
+
 switch (uname)
   case Darwin
     source ~/.iterm2_shell_integration.fish
+    alias vim = "mvim"
+    alias vi = "vim"
+    set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 end
 
-set -g theme_color_scheme solarized
-
-set -gx BD_OPT 'insensitive'
 
