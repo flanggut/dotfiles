5330bc4bf4936bc4230f8c9e8b352349ad799ffb
Fabian Langguth <flanggut@fb.com>
2018-08-02 13:40:35 +0200

    Update fzf hg

--- omf/functions/fzf_hg_commit.fish
+++ omf/functions/fzf_hg_commit.fish
@@ -2,7 +2,7 @@ function fzf_hg_commit
 
   hg sl --color always 2>/dev/null > /tmp/fzf.tmp
 
-  hg sl 2>/dev/null | sed -e 's/^[\|\ \/]*//' | egrep '(^o|^\@)' | cut -c 4-9 | fzf --ansi --no-sort --tiebreak=index --reverse --preview-window right:90% --preview "grep --color=always -E '{}|\$' /tmp/fzf.tmp" | read -l result
+  hg sl 2>/dev/null | sed -e 's/^[\|\ \/]*//' | egrep '(^o|^\@)' | cut -c 4-11 | fzf --ansi --no-sort --tiebreak=index --reverse --preview-window right:90% --preview "grep --color=always -E '{}|\$' /tmp/fzf.tmp" --bind "ctrl-u:execute(hg up {})" | read -l result
 
   if test $result
     commandline -it -- $result
