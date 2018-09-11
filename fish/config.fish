30d01ef56cd0326a6779f9f346a458d32d508034
Fabian Langguth <flanggut@fb.com>
2018-09-11 13:26:08 +0200

    Add bootstrap for fisherman

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,9 @@
+# Bootstrap fisherman
+if not test -f ~/.config/fish/functions/fisher.fish
+  curl -sLo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
+  fisher
+end
+
 set -g theme_color_scheme solarized
 set -g theme_date_format "+%H:%M:%S"
 
