59f9e15cf875d68f783cb126b93caccc16f1640e
Fabian Langguth <flanggut@fb.com>
2018-12-14 11:15:24 +0100

    Small change to fzf_hg

--- salmon/functions/__fzf_hg.fish
+++ salmon/functions/__fzf_hg.fish
@@ -1,8 +1,5 @@
 function __fzf_hg
 
-  # Create smartlog cache
-  set -l SLCACHE /tmp/fzf_hg_smartlog_cache.tmp
-
   # Extract hg commit hashes and run fzf to select commit. Selecting a commit prints hash on commandline.
   # Keybindings:
   # ENTER : hg update to given commit and exit
@@ -11,13 +8,20 @@ function __fzf_hg
   # CTRL-R: hg rebase -s commit -d master
   # CTRL-S: hg show commit
   # CTRL-Y: copy commit id to clipboard and exit
-  hg sl --color always 2>/dev/null > $SLCACHE
-  hg sl 2>/dev/null | sed -e 's/^[\|\ \/]*//' | egrep '(^o|^\@)' | cut -c 4-11 | \
-  fzf -e --ansi --no-sort --tiebreak=index --reverse --preview-window right:90% \
-  --preview "grep --color=always -E '{}|\$' $SLCACHE" \
+  hg sl --color always 2>/dev/null > ~/.cache/fzf_hg_smartlog
+
+  # Old version: only search of commit hash
+  # hg sl 2>/dev/null | sed -e 's/^[\|\ \/]*//' | egrep '(^o|^\@)' | cut -c 4-11 | \
+  # fzf -e --ansi --no-sort --tiebreak=index --reverse --preview-window right:90% \
+  # --preview "grep --color=always -E '{}|\$' ~/.cache/fzf_hg_smartlog" \
+
+  # New version: also search for commit message
+  hg sl --color always 2>/dev/null | sed -e 's/^[\|\ \/]*//' | sed '/^$/d' | paste - - | cut -c 4- | \
+  fzf --ansi --no-sort --reverse --preview-window right:90% \
+  --preview 'grep --color=always -E "$(echo {} | cut -c -8)|\$" ~/.cache/fzf_hg_smartlog'
   --bind "enter:execute(hg up {} && hg fsl --color always 2>/dev/null)+abort" \
-  --bind "ctrl-u:execute(hg up {} && hg sl --color always 2>/dev/null > $SLCACHE)+toggle-preview+toggle-preview" \
-  --bind "ctrl-g:execute(hg graft {} && hg sl --color always 2>/dev/null > $SLCACHE)+toggle-preview+toggle-preview" \
+  --bind "ctrl-u:execute(hg up {} && hg sl --color always 2>/dev/null > ~/.cache/fzf_hg_smartlog)+toggle-preview+toggle-preview" \
+  --bind "ctrl-g:execute(hg graft {} && hg sl --color always 2>/dev/null > ~/.cache/fzf_hg_smartlog)+toggle-preview+toggle-preview" \
   --bind "ctrl-r:execute(hg rebase -s {} -d master)+abort" \
   --bind "ctrl-s:execute(hg show --color always {} | less -R)" \
   --bind "ctrl-y:execute(printf {} | pbcopy)+abort"
