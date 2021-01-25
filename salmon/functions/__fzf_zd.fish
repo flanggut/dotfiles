2778483d4b56f43abbd7f784e32edf5879eb173e
Fabian Langguth <flanggut@fb.com>
2021-01-25 22:17:14 +0100

    fish: change order in fzf cd

--- salmon/functions/__fzf_zd.fish
+++ salmon/functions/__fzf_zd.fish
@@ -1,5 +1,5 @@
 function __fzf_zd
-  set dir (z -l $argv | cut -b12- | tac | fzf --reverse --preview 'tree -C {} | head -200' --preview-window down:50)
+  set dir (z -l $argv | cut -b12- | tac | fzf --tac --no-sort --preview 'tree -C {} | head -200' --preview-window down:40)
   if [ $dir ]
     cd $dir
   end
