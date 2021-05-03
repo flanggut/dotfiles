3a2be0d30f7c14a5ca286613e0d63d423673a7d4
Fabian Langguth <flanggut@fb.com>
2021-05-03 17:00:20 +0200

    raycast: add files in new location

---
+++ raycast/cleanshot-capture-area.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Capture Area
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ./images/capture-area.png
+# @raycast.packageName CleanShot X
+
+# Documentation:
+# @raycast.author CleanShot X
+# @raycast.authorURL https://twitter.com/CleanShot_app
+# @raycast.description Take a screenshot of a specific area on your screen.
+
+open "cleanshot://capture-area"
