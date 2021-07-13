d5078b79ae7902dc8c1df721b3117ca7f4c1b821
Fabian Langguth <flanggut@fb.com>
2021-07-13 11:56:47 +0200

    fish: cdd updates

--- salmon/conf.d/fish_user_key_bindings.fish
+++ salmon/conf.d/fish_user_key_bindings.fish
@@ -3,4 +3,5 @@ bind \ck __fzf_reverse_history_search
 bind \ch '__fzf_hg ; commandline -f repaint'
 bind \co __fzf_zd
 bind \cn n
+bind \cl cdd
 
--- salmon/functions/cdd.fish
+++ salmon/functions/cdd.fish
@@ -1,6 +1,7 @@
 function cdd
-  set dir (find -L . -mindepth 1 -maxdepth 4 -path '*/\.*' -prune -o -type d -print ^ /dev/null | cut -b3- | fzf --reverse --preview 'tree -C {} | head -200')
+  set dir (find -L . -mindepth 1 -maxdepth 1 -type d | cut -b3- | fzf --preview 'tree -C {} | head -200')
   if [ $dir ]
     cd $dir
   end
+  commandline -f repaint
 end
