8e1289f912b5ff77d73253745a2b548b9e732d2b
Fabian Langguth <flanggut@fb.com>
2021-10-06 21:18:11 +0200

    raycast: Add icon for new kitty script

--- raycast/new-kitty.sh
+++ raycast/new-kitty.sh
@@ -5,6 +5,9 @@
 # @raycast.title New kitty
 # @raycast.mode compact
 # @raycast.packageName Terminal
+
+# Optional parameters:
+# @raycast.icon images/kitty.png
 #
 # Documentation:
 # @raycast.description Open new kitty window
