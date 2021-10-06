7ed96c7b49cd9e243648b582f319cb67fd438ae6
Fabian Langguth <flanggut@fb.com>
2021-10-06 21:04:17 +0200

    raycast: script to open new kitty

---
+++ raycast/new-kitty.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title New kitty
+# @raycast.mode compact
+# @raycast.packageName Terminal
+#
+# Documentation:
+# @raycast.description Open new kitty window
+
+kitty --single-instance
