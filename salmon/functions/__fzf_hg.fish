605e03f2a5475204379843732cae746bc6860ed3
Fabian Langguth <flanggut@fb.com>
2021-11-18 15:00:04 +0100

    fish: fzf hg updates

--- salmon/functions/__fzf_hg.fish
+++ salmon/functions/__fzf_hg.fish
@@ -13,7 +13,7 @@ function __fzf_hg
   # New version: also search for commit message
   FZF_DEFAULT_COMMAND="hg sl --color always 2>/dev/null > ~/.cache/fzf_hg_smartlog && cat ~/.cache/fzf_hg_smartlog | sed -e 's/^[\|\/\ ╷│├╯╭─╯]*//' | sed -e 's/[0-9]*:[0-9]*//' | sed -e 's/at\ *//' | sed -e 's/D[0-9]*//' | egrep -A 1 '(^(o|\@)\ )' | sed '/^--/d' | paste - - | cut -c 4- | sed -e 's/\ flanggut//'" fzf \
   --ansi --no-sort --reverse --preview-window right:60% \
-  --preview "rg -N --passthru --color=always (echo {} | cut -c -9) ~/.cache/fzf_hg_smartlog" \
+  --preview "rg -N --passthru --color=always --colors 'match:bg:white' (echo {} | cut -c -9) ~/.cache/fzf_hg_smartlog" \
   --bind "enter:execute(hg up (echo {} | cut -c -9) && hg fsl --color always 2>/dev/null)+abort" \
   --bind "ctrl-e:execute-silent(hg hide (echo {} | cut -c -9))+reload($FZF_DEFAULT_COMMAND)+refresh-preview+clear-query" \
   --bind "ctrl-u:execute(hg up (echo {} | cut -c -9))+reload($FZF_DEFAULT_COMMAND)+refresh-preview+clear-query" \
@@ -21,8 +21,10 @@ function __fzf_hg
   --bind "ctrl-h:execute(hg rebase -s (echo {} | cut -c -9) -d . )+reload($FZF_DEFAULT_COMMAND)+refresh-preview+clear-query" \
   --bind "ctrl-p:execute(hg pull)+reload($FZF_DEFAULT_COMMAND)+refresh-preview" \
   --bind "ctrl-r:execute(hg rebase -s (echo {} | cut -c -9) -d . )+reload($FZF_DEFAULT_COMMAND)+refresh-preview+clear-query" \
-  --bind "ctrl-s:execute(hg show --color always (echo {} | cut -c -9) | less -R)" \
+  --bind "ctrl-o:execute(hg show --color always (echo {} | cut -c -9) | less -R)" \
+  --bind "ctrl-s:execute(jf s -n -r (echo {} | cut -c -9))+execute(hg show --stat (echo {} | cut -c -9))+abort" \
   --bind "ctrl-t:execute(hg rebase -s (pbpaste) -d (echo {} | cut -c -9))+reload($FZF_DEFAULT_COMMAND)+refresh-preview+clear-query" \
+  --bind "ctrl-v:execute(hg show --stat (echo {} | cut -c -9) | grep https | sed 's/.*https/https/' | xargs open)+clear-query" \
   --bind "ctrl-y:execute-silent(printf (echo {} | cut -c -9) | pbcopy)"
 
   # Reprint the command line
