1ae30ee2960669b4b28929358279fdcda7f10ccd
Fabian Langguth <flanggut@fb.com>
2021-11-24 21:14:42 +0100

    raycast: add speedtest script

---
+++ raycast/speedtest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Speedtest
+# @raycast.mode fullOutput
+# @raycast.packageName Terminal
+
+# Documentation:
+# @raycast.description Run speedtest using macOS tool networkQuality
+
+networkQuality
