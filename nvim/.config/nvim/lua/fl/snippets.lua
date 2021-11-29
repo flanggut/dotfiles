04be4d66f3d32901eb6f09758014714d14902265
Fabian Langguth <flanggut@fb.com>
2021-11-29 15:34:22 +0100

    vim: retab snippets

--- nvim/.config/nvim/lua/fl/snippets.lua
+++ nvim/.config/nvim/lua/fl/snippets.lua
@@ -3,25 +3,29 @@ local ls  = require("luasnip")
 local M = {}
 
 M.snippets = {
-	cpp = {
+  cpp = {
     -- cout
-		ls.snippet("co", { -- Trigger is co.
-			-- Simple static text.
-			ls.text_node('std::cout << "\\U0001F98A " << '),
-			-- Insert node.
-			ls.insert_node(0),
-			ls.text_node(' << std::endl;'),
-		}),
+    ls.snippet("co", { -- Trigger is co.
+      -- Simple static text.
+      ls.text_node('std::cout << "\\U0001F98A " << '),
+      -- Insert node.
+      ls.insert_node(0),
+      ls.text_node(' << std::endl;'),
+    }),
     -- lambda
-		ls.snippet("la", {
-			ls.text_node('[&'),
-			ls.insert_node(1),
-			ls.text_node(']('),
-			ls.insert_node(2),
-			ls.text_node('){'),
-			ls.insert_node(0),
-			ls.text_node('}'),
-		}),
+    ls.snippet("la", {
+      ls.text_node('[&'),
+      ls.insert_node(1),
+      ls.text_node(']('),
+      ls.insert_node(2),
+      ls.text_node('){'),
+      ls.insert_node(0),
+      ls.text_node('}'),
+    }),
+    -- maybe unused
+    ls.snippet("may", {
+      ls.text_node('[[maybe_unused]]'),
+    }),
   },
 }
 
