eebf5b314aa9cb0f86607811f88c4bd38fc2184d
Fabian Langguth <flanggut@fb.com>
2021-12-02 12:01:00 +0100

    fish: fix fzf history search

--- salmon/functions/__fzf_reverse_history_search.fish
+++ salmon/functions/__fzf_reverse_history_search.fish
@@ -1,6 +1,7 @@
 function __fzf_reverse_history_search
-  history -z | fzf --read0 --print0 -m --tiebreak=index --bind "ctrl-y:execute(printf {} | pbcopy)+abort" | read -lz result
-  and commandline -- $result
+  history -z | fzf --read0 --print0 -m --tiebreak=index --bind "ctrl-y:execute(printf {} | pbcopy)+abort" | while read -lz i; set command "$command""$i"' ; and '; end
+  set command (echo $command | rev | cut -c7- | rev)
+  commandline -- $command
   commandline -f repaint
 end
 
