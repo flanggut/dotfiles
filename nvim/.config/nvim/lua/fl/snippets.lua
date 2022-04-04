cb6c9c3645ca96cade26c9b60b145c224d217e69
Fabian Langguth <flanggut@fb.com>
2022-04-04 15:58:09 +0200

    vim: remove use of deprecated luasnip api

--- nvim/.config/nvim/lua/fl/snippets.lua
+++ nvim/.config/nvim/lua/fl/snippets.lua
@@ -7,21 +7,16 @@ function M.load()
   local i = ls.insert_node
   local rep = require("luasnip.extras").rep
 
-  local snippets = {
-    cpp = {
-      s("co", fmt('std::cout << "\\U0001F98A " << {} << std::endl;', { i(1) })),
-      s("la", fmt('[&]({}){{ {} }}', { i(0), i(1) })),
-      s("imfor", fmt('for (int y = 0; y < {}.height(); ++y) {{\n  for (int x = 0; x < {}.width(); ++x) {{\n    {}\n  }}\n}}', { i(1), rep(1), i(0) })),
-      s("may", fmt('[[maybe_unused]]', {})),
-    },
-    lua = {
-    },
-    python = {
-      s("print", fmt('print(f"{{ {} }}")', { i(0) })),
-    },
-  }
+  ls.add_snippets("cpp", {
+    s("co", fmt('std::cout << "\\U0001F98A " << {} << std::endl;', { i(1) })),
+    s("la", fmt('[&]({}){{ {} }}', { i(0), i(1) })),
+    s("imfor", fmt('for (int y = 0; y < {}.height(); ++y) {{\n  for (int x = 0; x < {}.width(); ++x) {{\n    {}\n  }}\n}}', { i(1), rep(1), i(0) })),
+    s("may", fmt('[[maybe_unused]]', {})),
+  })
 
-  ls.snippets = snippets
+  ls.add_snippets("python", {
+    s("print", fmt('print(f"{{ {} }}")', { i(0) })),
+  })
 
   require'cmp_luasnip'.clear_cache()
 end
