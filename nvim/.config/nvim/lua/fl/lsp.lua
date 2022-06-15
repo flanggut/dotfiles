2ac65a877e2b8de373280563ae6365655fe3cb53
Fabian Langguth <fabian.langguth@gmail.com>
2022-06-15 17:00:36 +0200

    vim: lsp configuration updates

--- nvim/.config/nvim/lua/fl/lsp.lua
+++ nvim/.config/nvim/lua/fl/lsp.lua
@@ -1,14 +1,15 @@
-local nvim_lsp = require ('lspconfig')
+local nvim_lsp = require('lspconfig')
 local util = require('lspconfig.util')
 
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Mappings.
-  local opts = { noremap=true, silent=true }
+  local opts = { noremap = true, silent = true }
   buf_set_keymap('n', '<leader>j', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
   buf_set_keymap('n', '<C-j>', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
   buf_set_keymap('i', '<C-h>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
@@ -16,7 +17,8 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<leader>sa', "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
   buf_set_keymap('n', '<leader>sr', "<cmd>lua require'telescope.builtin'.lsp_references()<cr>", opts)
-  buf_set_keymap('n', '<leader>sy', "<cmd>lua require'telescope.builtin'.lsp_document_symbols({symbol_width = 50, symbol_type_width = 12})<cr>", opts)
+  buf_set_keymap('n', '<leader>sy',
+    "<cmd>lua require'telescope.builtin'.lsp_document_symbols({symbol_width = 50, symbol_type_width = 12})<cr>", opts)
   buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
 
   -- Set some keybinds conditional on server capabilities
@@ -32,8 +34,8 @@ local on_attach = function(client, bufnr)
 
   vim.cmd("highlight LspDiagnosticsVirtualTextError guifg='#EEEEEE'")
 
-  require'lsp_signature'.on_attach({bind = true, floating_window = false, hint_prefix = '  '})
-  require'illuminate'.on_attach(client)
+  require 'lsp_signature'.on_attach({ bind = true, floating_window = false, hint_prefix = '  ' })
+  require 'illuminate'.on_attach(client)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -55,6 +57,15 @@ nvim_lsp['rust_analyzer'].setup {
   on_attach = on_attach
 }
 
+-- BUCK
+local found_buckls_config, _ = pcall(require, 'lspconfig.server_configurations.buckls')
+if found_buckls_config then
+  nvim_lsp['buckls'].setup {
+    capabilities = capabilities,
+    on_attach = on_attach
+  }
+end
+
 -- Python
 if not string.find(vim.fn.expand(vim.loop.cwd()), "fbsource") then
   nvim_lsp['pyright'].setup {
@@ -69,48 +80,52 @@ else
   vim.g.python3_host_prog = '/usr/local/bin/python3'
   vim.g.python_host_prog = '/usr/local/bin/python2'
 
-  nvim_lsp['pylsp'].setup {
-    cmd = {
-      vim.env.PYLS_PATH,
-      "--verbose",
-      "--log-file",
-      "/tmp/flanggut-logs/pyls.log"
-    },
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = {"python", "buck"},
-    root_dir = util.root_pattern(".buckconfig"),
-    single_file_support = true,
-    settings = {
-      pyls = {
-        formatAlreadyFormattedFilesOnSave = false,
-        BuckFormatOnSave = false,
-        extraPaths = {},
-        ThriftGoToDef = true,
-        plugins = {
-          jedi_completion = {enabled = true},
-          jedi_definition = {
-            enabled = true,
-            follow_imports = true,
-            follow_builtin_imports = true
-          },
-          jedi_hover = {enabled = true},
-          jedi_references = {enabled = true},
-          jedi_signature_help = {enabled = true},
-          jedi_symbols = {enabled = true},
-          fb_pyfmt_format = {enabled = true}
-        }
+  if (vim.env.PYLS_PATH == nil or vim.env.PYLS_PATH == '') then
+    vim.notify("$PYLS_PATH not defined in environment. Cannot start python LSP.", "error")
+  else
+    nvim_lsp['pylsp'].setup {
+      cmd = {
+        vim.env.PYLS_PATH,
+        "--verbose",
+        "--log-file",
+        "/tmp/flanggut-logs/pyls.log"
       },
-      completionDetailLimit = 50,
-    },
-    handlers = {
-      ['window/showStatus'] = vim.lsp.handlers["window/showMessage"]
-    },
-  }
+      on_attach = on_attach,
+      capabilities = capabilities,
+      filetypes = { "python" },
+      root_dir = util.root_pattern(".buckconfig"),
+      single_file_support = false,
+      settings = {
+        pyls = {
+          formatAlreadyFormattedFilesOnSave = false,
+          BuckFormatOnSave = false,
+          extraPaths = {},
+          ThriftGoToDef = true,
+          plugins = {
+            jedi_completion = { enabled = true },
+            jedi_definition = {
+              enabled = true,
+              follow_imports = true,
+              follow_builtin_imports = true
+            },
+            jedi_hover = { enabled = true },
+            jedi_references = { enabled = true },
+            jedi_signature_help = { enabled = true },
+            jedi_symbols = { enabled = true },
+            fb_pyfmt_format = { enabled = true }
+          }
+        },
+        completionDetailLimit = 50,
+      },
+      handlers = {
+        ['window/showStatus'] = vim.lsp.handlers["window/showMessage"]
+      },
+    }
+  end
 end
 
 -- setup lua
-nvim_lsp['sumneko_lua'].setup(require('lua-dev').setup{
+nvim_lsp['sumneko_lua'].setup(require('lua-dev').setup {
   lspconfig = {
     capabilities = capabilities,
     on_attach = on_attach
