b1cf3b16e72f11a16e600ace2f36f7fde3e97004
Fabian Langguth <fabian.langguth@gmail.com>
2024-02-20 09:43:38 +0100

    kitty: diff hint update

--- kitty/.config/kitty/diff-hint.py
+++ kitty/.config/kitty/diff-hint.py
@@ -4,23 +4,25 @@ import shlex
 import os
 import subprocess
 
-DIFF_COMMAND = 'open https://www.internalfb.com/diff/{diff}'
-DIFF_REGEX = r'(D[\d]+)'
+COMMAND = "open 'https://www.internalfb.com/intern/bunny/?q={paste}'"
+REGEX = r"([DPT][\d]+)"
+
 
 def mark(text, args, Mark, extra_cli_args, *a):
-    for idx, m in enumerate(re.finditer(DIFF_REGEX, text)):
+    for idx, m in enumerate(re.finditer(REGEX, text)):
         start, end = m.span()
-        mark_text = text[start:end].replace('\n', '').replace('\0', '')
-        yield Mark(idx, start, end, mark_text, {"type": "diff"})
+        mark_text = text[start:end].replace("\n", "").replace("\0", "")
+        yield Mark(idx, start, end, mark_text, {"type": "bunny"})
+
 
 def handle_result(args, data, target_window_id, boss, extra_cli_args, *a):
     matches, groupdicts = [], []
 
-    for m, g in zip(data['match'], data['groupdicts']):
+    for m, g in zip(data["match"], data["groupdicts"]):
         if m:
             matches.append(m), groupdicts.append(g)
 
     for word, match_data in zip(matches, groupdicts):
-        if "type" in match_data and match_data["type"] == "diff":
-            command = DIFF_COMMAND.replace('{diff}', word)
+        if "type" in match_data and match_data["type"] == "bunny":
+            command = COMMAND.replace("{paste}", word)
             subprocess.call(shlex.split(command))
