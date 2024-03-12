5889d7c213019ca388feb679c081ff9e5f49d8c3
Fabian Langguth <fabian.langguth@gmail.com>
2024-03-12 11:34:40 +0100

    vim: plugin updates

--- nvim/.config/nvim/lua/fl/lazy/plugins/ui.lua
+++ nvim/.config/nvim/lua/fl/lazy/plugins/ui.lua
@@ -44,6 +44,14 @@ return {
     end,
   },
 
+  {
+    "mvllow/modes.nvim",
+    tag = "v0.2.0",
+    config = function()
+      require("modes").setup()
+    end,
+  },
+
   -- noice
   {
     "folke/noice.nvim",
