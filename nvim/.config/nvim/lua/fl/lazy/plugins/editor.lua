884a48cab1cf734889f96804cc949dfa1c52c256
Fabian Langguth <fabian.langguth@gmail.com>
2024-07-29 09:56:30 +0200

    nvim: Add ollama gen

--- nvim/.config/nvim/lua/fl/lazy/plugins/editor.lua
+++ nvim/.config/nvim/lua/fl/lazy/plugins/editor.lua
@@ -584,35 +584,44 @@ return {
   {
     "David-Kunz/gen.nvim",
     cmd = { "Gen" },
-    opts = {
-      model = "llama3.1:8b", -- The default model to use.
-      host = "localhost", -- The host running the Ollama service.
-      port = "11434", -- The port on which the Ollama service is listening.
-      quit_map = "q", -- set keymap for close the response window
-      retry_map = "<c-r>", -- set keymap to re-send the current prompt
-      init = function(_)
-        pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
-      end,
-      -- Function to initialize Ollama
-      command = function(options)
-        ---@diagnostic disable-next-line: unused-local
-        local body = { model = options.model, stream = true }
-        return "curl --silent --no-buffer -X POST http://"
-          .. options.host
-          .. ":"
-          .. options.port
-          .. "/api/chat -d $body"
-      end,
-      -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
-      -- This can also be a command string.
-      -- The executed command must return a JSON object with { response, context }
-      -- (context property is optional).
-      -- list_models = '<omitted lua function>', -- Retrieves a list of model names
-      display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split".
-      show_prompt = false, -- Shows the prompt submitted to Ollama.
-      show_model = false, -- Displays which model you are using at the beginning of your chat session.
-      no_auto_close = false, -- Never closes the window automatically.
-      debug = false, -- Prints errors and the command which is run.
+    keys = {
+      { "<leader>og", "<cmd>Gen Generate_Code<cr>" },
     },
+    config = function()
+      local opts = {
+        model = "llama3.1:8b", -- The default model to use.
+        host = "localhost", -- The host running the Ollama service.
+        port = "11434", -- The port on which the Ollama service is listening.
+        quit_map = "q", -- set keymap for close the response window
+        retry_map = "<c-r>", -- set keymap to re-send the current prompt
+        init = function(_)
+          pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
+        end,
+        -- Function to initialize Ollama
+        command = function(options)
+          ---@diagnostic disable-next-line: unused-local
+          local body = { model = options.model, stream = true }
+          return "curl --silent --no-buffer -X POST http://"
+            .. options.host
+            .. ":"
+            .. options.port
+            .. "/api/chat -d $body"
+        end,
+        -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
+        -- This can also be a command string.
+        -- The executed command must return a JSON object with { response, context }
+        -- (context property is optional).
+        -- list_models = '<omitted lua function>', -- Retrieves a list of model names
+        display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split".
+        show_prompt = false, -- Shows the prompt submitted to Ollama.
+        show_model = false, -- Displays which model you are using at the beginning of your chat session.
+        no_auto_close = false, -- Never closes the window automatically.
+        debug = false, -- Prints errors and the command which is run.
+      }
+      require("gen").setup(opts)
+      require("gen").prompts["Generate_Code"] = {
+        prompt = "Generate code for the following problem: $input. Only ouput the result in format ```$filetype\n...\n```",
+      }
+    end,
   },
 }
