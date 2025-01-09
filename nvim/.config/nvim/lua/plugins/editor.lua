6554c357e9e99282cba2bd122b1667d9b80f0cbf
Fabian Langguth <fabian.langguth@gmail.com>
2025-01-09 23:53:18 +0100

    vim: cleanup surround

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -122,23 +122,6 @@ return {
     },
   },
 
-  -- surround
-  {
-    "echasnovski/mini.surround",
-    lazy = false,
-    opts = {
-      mappings = {
-        add = "gsa",            -- Add surrounding in Normal and Visual modes
-        delete = "gsd",         -- Delete surrounding
-        find = "gsf",           -- Find surrounding (to the right)
-        find_left = "gsF",      -- Find surrounding (to the left)
-        highlight = "gsh",      -- Highlight surrounding
-        replace = "gsr",        -- Replace surrounding
-        update_n_lines = "gsn", -- Update `n_lines`
-      },
-    },
-  },
-
   -- search/replace in multiple files
   {
     "MagicDuck/grug-far.nvim",
